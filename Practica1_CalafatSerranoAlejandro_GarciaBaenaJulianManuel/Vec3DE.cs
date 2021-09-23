5aac8e56d65d3db6f534ec38a4e496697d6df1fd
Julian <julian.garcia@cesjuanpablosegundo.es>
2021-09-23 18:09:23 +0200

    He cambiado el apartado de Vec3DE

--- Practica1_CalafatSerranoAlejandro_GarciaBaenaJulianManuel/Vec3DE.cs
+++ Practica1_CalafatSerranoAlejandro_GarciaBaenaJulianManuel/Vec3DE.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace Practica1_CalafatSerranoAlejandro_GarciaBaenaJulianManuel
 {
-    class Vec3DE
+    class Vec3DE : Vectores   
     {
+        public override void ImprimirPorPantalla(double[] vector)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double[] LeerVector(double[] vector)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double[] SumarVectores(double[] vector1, double[] vector2)
+        {
+            Console.WriteLine("Hola");
+            return vector1;
+        }
     }
 }
